89f617f379be83f712195f3e2d5f7d429aedb280
ArnavPednekar <arnavpednekar11@gmail.com>
2026-02-17 21:32:51 +0530

    initial commit did like the basic memory allocation and 3 functions

---
+++ chip8.cpp
@@ -0,0 +1,97 @@
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+class chip8 {
+public:
+  unsigned short opcode;
+  unsigned char memory[4096];
+  unsigned char v[16];
+  unsigned short index;
+  unsigned short pc;
+  unsigned char gfx[64 * 32];
+  unsigned char delay_timer, sound_timer;
+  unsigned short stack[16];
+  unsigned short sp;
+  unsigned char key[16];
+  unsigned char fontset[80] = {
+      0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+      0x20, 0x60, 0x20, 0x20, 0x70, // 1
+      0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+      0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+      0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+      0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+      0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+      0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+      0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+      0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+      0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+      0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+      0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+      0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+      0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+      0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+  };
+
+  // just remember who you are
+  void initailize() {
+    pc = 0x200;
+    index = 0;
+    sp = 0;
+    delay_timer = 0;
+    sound_timer = 0;
+
+    memset(memory, 0, sizeof(stack));
+    memset(v, 0, sizeof(v));
+    memset(stack, 0, sizeof(stack));
+    memset(gfx, 0, sizeof(gfx));
+
+    for (int i = 0; i < 80; ++i) {
+      memory[i] = fontset[i];
+    }
+  }
+  // emulation cycle
+  void cycle() {
+    opcode = memory[pc] << 8 | memory[pc + 1];
+
+    switch (opcode & 0xF000) {
+    case 0x0000:
+      if (opcode == 0x00E0) // cls
+        for (int i = 0; i < 64 * 32; i++) {
+          gfx[i] = 0;
+        }
+      else if (opcode == 0x00EE) {
+        stack[sp] = pc;
+        sp--;
+      } else if (opcode == 0x0000) {
+        uint16_t address = opcode & 0x0FFF;
+        pc = address;
+        break;
+      }
+    case 0x1000: // jp addr
+      uint16_t address = opcode & 0x0FFF;
+      pc = address;
+      break;
+    case 0x2000: // call addr
+      uint16_t address = opcode & 0x0FFF;
+      sp++;
+      stack[sp] = pc;
+      pc = address;
+      break;
+    default:
+      cout << "unknown opcode: 0x" << opcode << "\n" << endl;
+    }
+    // Update timers
+    if (delay_timer > 0) {
+      --delay_timer;
+    }
+
+    if (sound_timer > 0) {
+      if (sound_timer == 1) {
+        cout << ("BEEP!\n");
+        --sound_timer;
+      }
+    }
+  }
+};
