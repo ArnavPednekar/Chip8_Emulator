100201c21e710682c5ea428572bfd5a8ac7dca86
ArnavPednekar <arnavpednekar11@gmail.com>
2026-02-18 16:55:40 +0530

    added instructions till 8xy7 not added 8xy7 also imma put a pdf of  the Technical Reference that i am using

--- chip8.cpp
+++ chip8.cpp
@@ -56,7 +56,7 @@ public:
     opcode = memory[pc] << 8 | memory[pc + 1];
 
     switch (opcode & 0xF000) {
-    case 0x0000:
+    case 0x0000: {
       if (opcode == 0x00E0) // cls
         for (int i = 0; i < 64 * 32; i++) {
           gfx[i] = 0;
@@ -65,22 +65,103 @@ public:
         stack[sp] = pc;
         sp--;
       } else if (opcode == 0x0000) {
-        uint16_t address = opcode & 0x0FFF;
-        pc = address;
-        break;
+        pc = opcode & 0x0FFF;
       }
-    case 0x1000: // jp addr
+      break;
+    }
+    case 0x1000: { // jp addr
       uint16_t address = opcode & 0x0FFF;
       pc = address;
       break;
-    case 0x2000: // call addr
-      uint16_t address = opcode & 0x0FFF;
+    }
+    case 0x2000: { // call addr
       sp++;
       stack[sp] = pc;
-      pc = address;
+      pc = opcode & 0x0FFF;
       break;
+    }
+    case 0x3000: { // 3xkk - SE Vx, byte
+      uint8_t x = (opcode & 0x0F00) >> 8;
+      uint8_t kk = (opcode & 0x00FF);
+      if (v[x] == kk) { // Skip next instruction if Vx = kk.
+        pc += 2;
+      }
+      break;
+    }
+    case 0x4000: { // 4xkk - SNE Vx, byte
+      uint8_t x = (opcode & 0x0F00) >> 8;
+      uint8_t kk = (opcode & 0x00FF);
+      if (v[x] != kk) { // Skip next instruction if Vx != kk.
+        pc += 2;
+      }
+    }
+    case 0x5000: { // 5xy0 - SE Vx, Vy
+      uint8_t x = (opcode & 0x0F00) >> 8;
+      uint8_t y = (opcode & 0x00F0) >> 8;
+      if (v[x] == v[y]) { // Skip next instruction if Vx = Vy.
+        pc += 2;
+      }
+    }
+    case 0x6000: { // 6xkk - LD Vx , byte
+      uint8_t x = (opcode & 0x0F00) >> 8;
+      uint8_t kk = (opcode & 0x00FF);
+      v[x] = kk;
+    }
+    case 0x7000: { // 7xkk - ADD Vx, byte
+      uint8_t x = (opcode & 0x0F00) >> 8;
+      uint8_t kk = (opcode & 0x00FF);
+      v[x] = v[x] + kk;
+    }
+    case 0x8000: { // 8xy0 - LD Vx , Vy
+      uint8_t x = (opcode & 0x0F00) >> 8;
+      uint8_t y = (opcode & 0x00F0) >> 8;
+      switch (opcode & 0x000F) {
+      case 0x0: // 8xy0 -LD Vx, Vy
+        v[x] = v[y];
+        break;
+      case 0x1: // 8xy0 -LD Vx, Vy
+        v[x] |= v[y];
+        break;
+      case 0x2: // 8xy0 -LD Vx, Vy
+        v[x] &= v[y];
+        break;
+      case 0x3:
+        v[x] ^= v[y];
+        break;
+      case 0x4: { // 8xy4: ADD Vx, Vy
+        uint16_t sum = v[x] + v[y];
+        // If the sum is greater than 255 (0xFF), there is a carry
+        if (sum > 0xFF) {
+          v[0xF] = 1;
+        } else {
+          v[0xF] = 0;
+        }
+        // Keep only the lowest 8 bits and store in Vx
+        v[x] = (sum & 0xFF);
+        break;
+      }
+      case 0x5: { // 8xy5 - SUB Vx, Vy
+        if (v[y] > v[x]) {
+          v[0xF] = 1;
+        } else {
+          v[0xF] = 0;
+          v[x] = v[x] - v[y];
+        }
+        break;
+      }
+      case 0x6: { // 8xy6 - SHR Vx {, Vy}
+        uint8_t lsb = (v[x] & 0x01);
+        v[0xF] = lsb;
+        v[x] >>= 1;
+        break;
+      }
+      case 0x7: {
+      }
+      }
+    }
     default:
       cout << "unknown opcode: 0x" << opcode << "\n" << endl;
+      break;
     }
     // Update timers
     if (delay_timer > 0) {
